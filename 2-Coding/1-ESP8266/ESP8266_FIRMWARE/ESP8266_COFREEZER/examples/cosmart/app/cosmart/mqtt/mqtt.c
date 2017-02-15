c795fb99d634a1f37481d9741aecfc1d9c75b74d
Cocoonshu <shujian@oppo.com>
2017-02-15 13:48:41 +0800

    initialize project

---
+++ 2-Coding/1-ESP8266/ESP8266_FIRMWARE/ESP8266_COFREEZER/examples/cosmart/app/cosmart/mqtt/mqtt.c
@@ -0,0 +1,465 @@
+/* mqtt.c
+*  Protocol: http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/os/mqtt-v3.1.1-os.html
+*
+* Copyright (c) 2014-2015, Tuan PM <tuanpm at live dot com>
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* * Redistributions of source code must retain the above copyright notice,
+* this list of conditions and the following disclaimer.
+* * Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+* * Neither the name of Redis nor the names of its contributors may be used
+* to endorse or promote products derived from this software without
+* specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "cosmart/mqtt/mqtt.h"
+#include "cosmart/wifimanager.h"
+#include "cosmart/timer.h"
+#include "cosmart/config.h"
+#include "user_interface.h"
+#include "osapi.h"
+#include "mem.h"
+
+const char* MQTT_SUBSCRIBE_LIST[] = {
+		"dev/oled12864/display"
+};
+
+#ifndef DELETE_POINTER
+#define DELETE_POINTER(ptr) if (ptr != NULL) {os_free(ptr); ptr = NULL;}
+#endif
+#ifndef MQTT_Task
+#define MQTT_Task onSystemTask
+#endif
+
+typedef struct espconn Connnection;
+
+LOCAL void ping();
+LOCAL void unping();
+LOCAL void sendStream(MQTTClient* client);
+LOCAL void onSystemTask(os_event_t *event);
+LOCAL void onDNSResponding(const char* name, ip_addr_t* ip, void* args);
+LOCAL void onTCPConnected(void* args);
+LOCAL void onTCPReconnected(void* args, sint8 error);
+LOCAL void onTCPDisconnected(void* args);
+LOCAL void onTCPSent(void* args);
+LOCAL void onTCPReceived(void *args, char *data, unsigned short length);
+LOCAL void onPingTimeout(void *arg);
+
+LOCAL os_event_t  mTaskQueue[MQTT_TASK_QUEUE_SIZE];
+LOCAL MQTTClient* mClient    = NULL;
+LOCAL Timer*      mPingTimer = NULL;
+
+void ICACHE_FLASH_ATTR MQTT_initialize() {
+	Log_printfln("");
+	Log_printfln("[MQTT] MQTT initialized");
+
+	mClient = (MQTTClient*) os_malloc(sizeof(MQTTClient));
+	os_memset(mClient, NULL, sizeof(MQTTClient));
+	MQTT_setService(MQTT_SERVER, MQTT_ENABLE_SSL ? MQTT_SSL_PORT : MQTT_PORT);
+	MQTT_setConnectParameters(MQTT_PROTOCOL_LEVEL_3_1_1, MQTT_KEEP_ALIVE, MQTT_QoS, MQTT_KEEP_SESSOIN, MQTT_ENABLE_SSL);
+	MQTT_setAuthentication(MQTT_getClientIndentifier(), MQTT_USERNAME, MQTT_PASSWORD);
+	MQTT_setWill(MQTT_WILL_TOPIC, MQTT_WILL_MESSAGE);
+
+	system_os_task(MQTT_Task, MQTT_TASK_PRIORITY, mTaskQueue, MQTT_TASK_QUEUE_SIZE);
+}
+
+const char* ICACHE_FLASH_ATTR MQTT_getClientIndentifier() {
+	return WiFi_generateSTAIdentify();
+}
+
+void ICACHE_FLASH_ATTR MQTT_setService(const char* server, uint32 port) {
+	mClient->server = server;
+	mClient->port   = port;
+}
+
+void ICACHE_FLASH_ATTR MQTT_setConnectParameters(uint8 protocolLevel, uint16 keepAlive, uint8 QoS, bool isCleanSession,  bool isSSL) {
+	mClient->protocolLevel  = MQTT_PROTOCOL_LEVEL_3_1_1;
+	mClient->keepAlive      = keepAlive;
+	mClient->QoS            = QoS;
+	mClient->isCleanSession = isCleanSession;
+	mClient->enabledSSL     = isSSL;
+}
+
+void ICACHE_FLASH_ATTR MQTT_setAuthentication(const char* clientId, const char* username, const char* password) {
+	mClient->clientId = clientId;
+	mClient->username = username;
+	mClient->password = password;
+}
+
+void ICACHE_FLASH_ATTR MQTT_setWill(const char* willTopic, const char* willMessage) {
+	mClient->willTopic   = willTopic;
+	mClient->willMessage = willMessage;
+}
+
+LOCAL ICACHE_FLASH_ATTR void onSystemTask(os_event_t *event) {
+	MQTTClient* client = (MQTTClient*)(event->par);
+	if (client == NULL) {
+		return;
+	}
+
+	switch (client->currentTask) {
+		case MQTT_TASK_NONE:
+			break;
+		case MQTT_TASK_DNS: {
+			Log_printf("[MQTT] Query host name %s", client->server);
+			sint8 result = espconn_gethostbyname(client->connection, client->server, &client->hostIP, onDNSResponding);
+			if (result == ESPCONN_OK) {
+				Log_printfln(" - OK");
+				onDNSResponding(client->server, &client->hostIP, (void*) client->connection);
+			} else if (result == ESPCONN_INPROGRESS){
+				client->currentTask = MQTT_TASK_DNSING;
+				Log_printfln(" - doing");
+			} else {
+				Log_printfln(" - failed, result = %d", result);
+			}
+		} break;
+
+		case MQTT_TASK_CONNECT: {
+			client->currentTask = MQTT_TASK_CONNECTING;
+			client->status = STATUS_CONNECTING;
+			if (client->enabledSSL) {
+				Log_printfln("[MQTT] Sent SSL connect to %s", client->server);
+				espconn_secure_connect(client->connection);
+			} else {
+				Log_printfln("[MQTT] Sent TCP connect to %s", client->server);
+				espconn_connect(client->connection);
+			}
+		} break;
+
+		case MQTT_TASK_ABORT_CONNECTION: {
+			espconn_disconnect(mClient->connection);
+			DELETE_POINTER(mClient->connection->proto.tcp);
+			DELETE_POINTER(mClient->connection);
+			if (mClient->protocolStream != NULL) {
+				DELETE_POINTER(mClient->protocolStream->fixedHeader);
+				DELETE_POINTER(mClient->protocolStream->variableHeader);
+				DELETE_POINTER(mClient->protocolStream->payload);
+			}
+			mClient->currentTask = MQTT_TASK_NONE;
+		} break;
+
+		case MQTT_TASK_RECONNECT: {
+			espconn_disconnect(mClient->connection);
+			DELETE_POINTER(mClient->connection->proto.tcp);
+			DELETE_POINTER(mClient->connection);
+			mClient->currentTask = MQTT_TASK_NONE;
+			MQTT_connect();
+		} break;
+	}
+}
+
+LOCAL ICACHE_FLASH_ATTR void onDNSResponding(const char* name, ip_addr_t* ip, void* args) {
+	struct espconn* connection = (struct espconn *)args;
+	MQTTClient*     client     = (MQTTClient *)connection->reverse;
+
+	if (client == NULL) {
+		return;
+	}
+	if (ip == NULL) {
+		Log_printfln("[MQTT] %s query failed, try again.", client->server);
+		client->currentTask = MQTT_TASK_DNS;
+		system_os_post(MQTT_TASK_PRIORITY, MQTT_TASK_SIGNAL, (os_param_t)mClient);
+		return;
+	} else {
+		uint8 ipAddress[4];
+		os_memcpy(ipAddress, &ip->addr, 4);
+		Log_printfln("[MQTT] Mapped %s to %d.%d.%d.%d",
+				name, ipAddress[0], ipAddress[1], ipAddress[2], ipAddress[3]);
+		Log_printfln("[MQTT] Connecting %d.%d.%d.%d",
+				ipAddress[0], ipAddress[1], ipAddress[2], ipAddress[3], mClient->port);
+
+		// Connect TCP server
+		os_memcpy(client->connection->proto.tcp->remote_ip, &ip->addr, 4);
+		client->currentTask = MQTT_TASK_CONNECT;
+		system_os_post(MQTT_TASK_PRIORITY, MQTT_TASK_SIGNAL, (os_param_t)mClient);
+	}
+}
+
+LOCAL ICACHE_FLASH_ATTR void onTCPConnected(void* args) {
+	Log_printfln("[MQTT][onTCPConnected]");
+	struct espconn* connection = (struct espconn *)args;
+	MQTTClient*     client     = (MQTTClient *)connection->reverse;
+
+	client->status = STATUS_IDLE;
+
+	if (client->protocolStream != NULL) {
+		DELETE_POINTER(client->protocolStream->fixedHeader);
+		DELETE_POINTER(client->protocolStream->variableHeader);
+		DELETE_POINTER(client->protocolStream->payload);
+	}
+	DELETE_POINTER(client->protocolStream);
+	client->protocolStream = (ProtocolStream*) os_malloc(sizeof(ProtocolStream));
+	os_memset(client->protocolStream, NULL, sizeof(ProtocolStream));
+	MQTTProcotol_encodeConnectPacket(
+			client->protocolStream,
+			client->clientId,
+			client->username,
+			client->password,
+			client->willTopic,
+			client->willMessage,
+			client->isCleanSession,
+			client->keepAlive);
+
+	sendStream(client);
+
+	DELETE_POINTER(client->protocolStream->fixedHeader);
+	DELETE_POINTER(client->protocolStream->variableHeader);
+	DELETE_POINTER(client->protocolStream->payload);
+	DELETE_POINTER(client->protocolStream);
+}
+
+LOCAL ICACHE_FLASH_ATTR void onTCPReconnected(void* args, sint8 error) {
+	Log_printf("[MQTT][onTCPReconnected] Error: ");
+	switch (error) {
+	case ESPCONN_TIMEOUT: {
+		Log_printfln("timeout");
+		// Timeout
+	} break;
+	case ESPCONN_ABRT: {
+		Log_printfln("connection aborted");
+		// Connection aborted
+	} break;
+	case ESPCONN_RST: {
+		Log_printfln("connection reset");
+		// Connection reset
+	} break;
+	case ESPCONN_CLSD: {
+		Log_printfln("connection closed");
+		// Connection closed
+	} break;
+	case ESPCONN_CONN: {
+		Log_printfln("not connected yet");
+		// Not connected
+	} break;
+	case ESPCONN_HANDSHAKE: {
+		Log_printfln("SSL handshake failed");
+		// SSL handshake failed
+	} break;
+	case ESPCONN_SSL_INVALID_DATA: {
+		Log_printfln("SSL application invalid");
+		// SSL application invalid
+	} break;
+	default: {
+		Log_printfln("%d", error);
+	} break;
+	}
+
+	MQTT_connect();
+}
+
+LOCAL ICACHE_FLASH_ATTR void onTCPDisconnected(void* args) {
+	Log_printfln("[MQTT][onTCPDisconnected]");
+	espconn_disconnect(mClient->connection);
+	DELETE_POINTER(mClient->connection->proto.tcp);
+	DELETE_POINTER(mClient->connection);
+
+	if (mClient->status != STATUS_DISCONNECTING) {
+		MQTT_connect();
+	} else {
+		mClient->status != STATUS_IDLE;
+	}
+}
+
+LOCAL ICACHE_FLASH_ATTR void sendStream(MQTTClient* client) {
+	Log_printfln("[sendStream] status = %d", client->status);
+	if (client->status == STATUS_CONNECTING || client->status == STATUS_DISCONNECTING) {
+		return;
+	}
+
+	mClient->status = STATUS_SENDING;
+	uint8 buffer[client->protocolStream->encodedStreamLength];
+	os_memcpy(buffer, client->protocolStream->encodedStream, client->protocolStream->encodedStreamLength);
+	sint8 result = espconn_send(client->connection, buffer, client->protocolStream->encodedStreamLength);
+	if (result == ESPCONN_MAXNUM) {
+		Log_printfln(" - Failed, buffer is full");
+	} else {
+		Log_printfln(" - OK");
+		int i;
+		Log_printf("       - ");
+		for (i = 0; i < mClient->protocolStream->encodedStreamLength; i++) {
+			Log_printf("%x ", *(mClient->protocolStream->encodedStream + i));
+		}
+		Log_printfln("[Total: %d]", mClient->protocolStream->encodedStreamLength);
+	}
+}
+
+LOCAL ICACHE_FLASH_ATTR void onTCPSent(void* args) {
+	Log_printfln("[MQTT][onTCPSent]");
+	struct espconn* connection = (struct espconn *)args;
+	MQTTClient*     client     = (MQTTClient *)connection->reverse;
+	if (client->status == STATUS_SENDING) {
+		client->status = STATUS_IDLE;
+		client->keepAliveTimeCount = MQTT_PING_TIMEOUT;
+	}
+}
+
+LOCAL ICACHE_FLASH_ATTR void onTCPReceived(void *args, char *data, unsigned short length) {
+	Log_printf("[MQTT][onTCPReceived] data = ");
+	int i;
+	for (i = 0; i < length; i++) {
+		Log_printf("%x ", *(data + i));
+	}
+	Log_printfln("[Total: %d]", length);
+
+	struct espconn* connection = (struct espconn *)args;
+	MQTTClient*     client     = (MQTTClient *)connection->reverse;
+
+	if (client != NULL) {
+		if (client->protocolStream != NULL) {
+			DELETE_POINTER(client->protocolStream->fixedHeader);
+			DELETE_POINTER(client->protocolStream->variableHeader);
+			DELETE_POINTER(client->protocolStream->payload);
+		}
+		DELETE_POINTER(client->protocolStream);
+	}
+
+	uint8 packetType = *(data + 0);
+	packetType = (packetType >> 4) & 0x0F;
+
+	switch (packetType) {
+		case MQTT_CONNECT_ACK: {
+			Log_printfln("[MQTT] Received [CONNECT_ACK] from %d.%d.%d.%d:%d",
+					connection->proto.tcp->remote_ip[0],
+					connection->proto.tcp->remote_ip[1],
+					connection->proto.tcp->remote_ip[2],
+					connection->proto.tcp->remote_ip[3],
+					connection->proto.tcp->remote_port);
+			client->currentTask = MQTT_TASK_NONE;
+			ping();
+		} break;
+
+		case MQTT_PING_ACK: {
+			Log_printfln("[MQTT] Received [PING_ACK] from %d.%d.%d.%d:%d",
+					connection->proto.tcp->remote_ip[0],
+					connection->proto.tcp->remote_ip[1],
+					connection->proto.tcp->remote_ip[2],
+					connection->proto.tcp->remote_ip[3],
+					connection->proto.tcp->remote_port);
+			client->currentTask = MQTT_TASK_NONE;
+		} break;
+	}
+
+}
+
+LOCAL ICACHE_FLASH_ATTR void onPingTimeout(void *arg) {
+	mClient->keepAliveTimeCount--;
+	Log_printfln("[MQTT][ping] #0 time = %d s", mClient->keepAliveTimeCount);
+	if (mClient->keepAliveTimeCount > 0) {
+		return;
+	} else if (mClient->keepAliveTimeCount < 0) {
+		mClient->keepAliveTimeCount = -1;
+	}
+
+	Log_printfln("[MQTT][ping] #1");
+	if (mClient->currentTask == MQTT_TASK_NONE && mClient->status == STATUS_IDLE) {
+		// Send PING to MQTT
+		Log_printfln("[MQTT][ping] #2");
+
+		// Send PING
+		if (mClient->protocolStream != NULL) {
+			DELETE_POINTER(mClient->protocolStream->fixedHeader);
+			DELETE_POINTER(mClient->protocolStream->variableHeader);
+			DELETE_POINTER(mClient->protocolStream->payload);
+		}
+		DELETE_POINTER(mClient->protocolStream);
+		mClient->protocolStream = (ProtocolStream*) os_malloc(sizeof(ProtocolStream));
+		os_memset(mClient->protocolStream, NULL, sizeof(ProtocolStream));
+		MQTTProcotol_encodePingPacket(mClient->protocolStream);
+
+		Log_printf("[MQTT] Sent [PING] to %s:%d", mClient->server, mClient->port);
+		sendStream(mClient);
+
+		DELETE_POINTER(mClient->protocolStream->fixedHeader);
+		DELETE_POINTER(mClient->protocolStream->variableHeader);
+		DELETE_POINTER(mClient->protocolStream->payload);
+		DELETE_POINTER(mClient->protocolStream);
+	} else {
+		Log_printfln("[MQTT] Task is busy, cancel ping this time");
+	}
+}
+
+LOCAL ICACHE_FLASH_ATTR void ping() {
+	if (mPingTimer == NULL) {
+		mPingTimer = (Timer*) os_malloc(sizeof(Timer));
+		os_memset(mPingTimer, NULL, sizeof(Timer));
+		Timer_setCallback(mPingTimer, onPingTimeout);
+		Timer_setInterval(mPingTimer, MQTT_PING_INTERVAL);
+		Timer_setLoopable(mPingTimer, true);
+		Timer_setTimerUnit(mPingTimer, MSTimer);
+		Timer_start(mPingTimer);
+	}
+}
+
+LOCAL ICACHE_FLASH_ATTR void unping() {
+	if (mPingTimer != NULL) {
+		Timer_stop(mPingTimer);
+		os_free(mPingTimer);
+		mPingTimer = NULL;
+	}
+}
+
+void ICACHE_FLASH_ATTR MQTT_connect() {
+	if (mClient->connection != NULL) {
+		remot_info* remoteInfo = NULL;
+		espconn_get_connection_info(mClient->connection, &remoteInfo, mClient->enabledSSL);
+		if (remoteInfo != NULL) {
+			if (remoteInfo->state != ESPCONN_NONE || remoteInfo->state != ESPCONN_CLOSE) {
+				return;
+			}
+		}
+	}
+
+	if (mClient->connection == NULL) {
+		mClient->connection = (Connnection* )os_zalloc(sizeof(Connnection));
+	}
+	mClient->connection->type  = ESPCONN_TCP;
+	mClient->connection->state = ESPCONN_NONE;
+
+	if (mClient->connection->proto.tcp == NULL) {
+		mClient->connection->proto.tcp = (esp_tcp *)os_zalloc(sizeof(esp_tcp));
+	}
+	mClient->connection->proto.tcp->local_port  = espconn_port();
+	mClient->connection->proto.tcp->remote_port = mClient->port;
+	mClient->connection->reverse = mClient;
+
+	espconn_regist_connectcb(mClient->connection, onTCPConnected);
+	espconn_regist_reconcb(mClient->connection, onTCPReconnected);
+	espconn_regist_sentcb(mClient->connection, onTCPSent);
+	espconn_regist_recvcb(mClient->connection, onTCPReceived);
+	espconn_regist_disconcb(mClient->connection, onTCPDisconnected);
+
+	Log_printfln("[MQTT] Connect to %s:%d", mClient->server, mClient->port);
+	mClient->currentTask = MQTT_TASK_DNS;
+	system_os_post(MQTT_TASK_PRIORITY, MQTT_TASK_SIGNAL, (os_param_t)mClient);
+}
+
+void ICACHE_FLASH_ATTR MQTT_disconnect() {
+	Log_printfln("[MQTT] Disconnect from %s:%d", mClient->server, mClient->port);
+
+	unping();
+	if (false/* WiFi down || TCP down */) {
+		mClient->currentTask = MQTT_TASK_ABORT_CONNECTION;
+		system_os_post(MQTT_TASK_PRIORITY, MQTT_TASK_SIGNAL, (os_param_t)mClient);
+	} else {
+		mClient->status = STATUS_DISCONNECTING;
+		mClient->currentTask = MQTT_TASK_DISCONNECT;
+		system_os_post(MQTT_TASK_PRIORITY, MQTT_TASK_SIGNAL, (os_param_t)mClient);
+	}
+}
---
+++ 2-Coding/1-ESP8266/ESP8266_FIRMWARE/ESP8266_COFREEZER/examples/cosmart/app/include/cosmart/mqtt/mqtt.h
@@ -0,0 +1,122 @@
+/*
+ * mqtt.h
+ *
+ *  Created on: 2016/10/28
+ *      Author: Cocoonshu
+ *
+ */
+
+#ifndef APP_INCLUDE_COSMART_MQTT_MQTT_H_
+#define APP_INCLUDE_COSMART_MQTT_MQTT_H_
+
+#include "c_types.h"
+#include "ip_addr.h"
+#include "espconn.h"
+#include "cosmart/mqtt/mqtt_protocol.h"
+
+#define STATUS_IDLE          0
+#define STATUS_SENDING       1
+#define STATUS_CONNECTING    2
+#define STATUS_DISCONNECTING 3
+
+#define MQTT_TASK_SIGNAL           0x10
+#define MQTT_TASK_PRIORITY         2
+#define MQTT_TASK_QUEUE_SIZE       2
+#define MQTT_TASK_NONE             0x00
+#define MQTT_TASK_DNS              0x01
+#define MQTT_TASK_DNSING           0x02
+#define MQTT_TASK_CONNECT          0x03
+#define MQTT_TASK_CONNECTING       0x04
+#define MQTT_TASK_DISCONNECT       0x05
+#define MQTT_TASK_DISCONNECTING    0x06
+#define MQTT_TASK_ABORT_CONNECTION 0x07
+#define MQTT_TASK_RECONNECT        0x08
+
+#define MQTT_PROTOCOL_LEVEL_3_1_1  0x04
+#define MAX_REMAIN_LENGTH          268435455
+#define MQTT_NAME                  "MQTT"
+#define MQTT_SERVER                "45.55.5.8" // "m13.cloudmqtt.com"
+#define MQTT_PORT                  1883 // 10419
+#define MQTT_SSL_PORT              20419
+#define MQTT_KEEP_ALIVE            120
+#define MQTT_QoS                   MQTT_QoS_AT_LEAST_ONCE
+#define MQTT_ENABLE_SSL            false
+#define MQTT_KEEP_SESSOIN          true
+#define MQTT_WILL_TOPIC            "dev/offline"
+#define MQTT_WILL_MESSAGE          "000000"
+#define MQTT_USERNAME              "Cocoonshu" // "ctytmovg"
+#define MQTT_PASSWORD              "89mik7" // "FI4fkpUG97nx"
+#define MQTT_PING_INTERVAL         1000
+#define MQTT_PING_TIMEOUT          (MQTT_KEEP_ALIVE / 2)
+
+enum QoS {
+	MQTT_QoS_AT_MOST_ONCE  = 0x00,
+	MQTT_QoS_AT_LEAST_ONCE = 0x01,
+	MQTT_QoS_ONLY_ONCE     = 0x02
+};
+
+enum ResponseCode {
+	RES_CODE_CONNECT_COMFIRMED            = 0x00,
+	RES_CODE_UNSUPPORTED_PROTOCOL_LEVEL   = 0x01,
+	RES_CODE_ILLEGAL_CLIENT_IDENTIFIER    = 0x02,
+	RES_CODE_SERVER_INVALIDATED           = 0x03,
+	RES_CODE_INVALID_USERNAME_OR_PASSWORD = 0x04,
+	RES_CODE_UNAUTHORIZATED               = 0x05
+};
+
+enum PacketType {
+	MQTT_CONNECT           = 1,
+	MQTT_CONNECT_ACK       = 2,
+	MQTT_PUBLISH           = 3,  // packet identifier
+	MQTT_PUBLISH_ACK       = 4,  // packet identifier
+	MQTT_PUBLISH_RECEVIED  = 5,  // packet identifier
+	MQTT_PUBLISH_RELEASE   = 6,  // packet identifier
+	MQTT_PUBLISH_COMPLETED = 7,  // packet identifier
+	MQTT_SUBSCRIBE         = 8,  // packet identifier
+	MQTT_SUBSCRIBE_ACK     = 9,  // packet identifier
+	MQTT_UNSUBSCRIBE       = 10, // packet identifier
+	MQTT_UNSUBSCRIBE_ACK   = 11, // packet identifier
+	MQTT_PING              = 12,
+	MQTT_PING_ACK          = 13,
+	MQTT_DISCONNECT        = 14
+};
+
+typedef struct {
+	const char* clientId;
+	const char* username;
+	const char* password;
+	const char* server;
+	uint32      port;
+	uint8       protocolLevel;
+	uint16      keepAlive;
+	uint8       QoS;
+	bool        isCleanSession;
+	bool        enabledSSL;
+	const char* willTopic;
+	const char* willMessage;
+
+	struct espconn* connection;
+	ip_addr_t       hostIP;
+	uint8           currentTask;
+	ProtocolStream* protocolStream;
+	sint8           status;
+	int             keepAliveTimeCount;
+} MQTTClient;
+
+typedef void OnMessageReceived(const char* topic, const char* message);
+
+void        MQTT_initialize();
+void        MQTT_setService(const char* server, uint32 port);
+void        MQTT_setConnectParameters(uint8 protocolLevel, uint16 keepAlive, uint8 QoS, bool isCleanSession, bool isSSL);
+void        MQTT_setAuthentication(const char* clientID, const char* username, const char* password);
+void        MQTT_setWill(const char* willTopic, const char* willMessage);
+void        MQTT_connect();
+void        MQTT_disconnect();
+void        MQTT_subscribe();
+void        MQTT_unsubscribe();
+void        MQTT_publish(const char* topic, const char* message);
+void        MQTT_setOnReceivedCallback(OnMessageReceived* listener);
+const char* MQTT_getClientIndentifier();
+
+
+#endif /* APP_INCLUDE_COSMART_MQTT_MQTT_H_ */
